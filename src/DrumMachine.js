a8830b62138485f1bce8d3b156f3e507346f51cd
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2021-01-11 09:33:22 -0300

    feat: improve style and add credicts

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,14 +1,21 @@
 import Drum from './Drum';
 import DrumSet from './DrumSet';
+import './DrumMachine.css';
 
 export function DrumMachine() {
     return (
-        <div>
-            {
-                DrumSet.map(drumItem => 
-                    <Drum key={drumItem.id} id={drumItem.id} label={drumItem.label} keyCode={drumItem.keyCode} sound={drumItem.sound}/>
-                )
-            }
+        <div className="container">
+            <div className="drum-set">
+                {
+                    DrumSet.map(drumItem => 
+                        <Drum key={drumItem.id} id={drumItem.id} label={drumItem.label} keyCode={drumItem.keyCode} sound={drumItem.sound}/>
+                    )
+                }
+            </div>
+            <div className="credicts">
+                Developed by 
+                <a href="https://github.com/thiagobitencourt/drum-machine" target="_blank">Thiago Bitencourt</a>
+            </div>
         </div>
     );
 }
